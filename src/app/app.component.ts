c9a768696f7857fd17d0aae5782817d503125313
AustineChi <emeghara.augustine@gmail.com>
2019-11-23 06:57:51 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+
+@NgModule(
+  {
+    imports: [
+      FormsModule
+    ]
+  }
+)
 
 @Component({
   selector: 'app-root',
@@ -6,5 +15,69 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  newNote : string;
+  noteId : number;
+  notes: any[];
   title = 'angular-todo-list';
+  editStatus: boolean = false;
+
+  constructor(){
+
+  }
+
+  ngOnInit(){
+    this.notes = [
+      {
+        id: 1,
+        note: "my first note"
+      },
+      {
+        id: 2,
+        note: "my second note"
+      },
+      {
+        id: 3,
+        note: "my third note"
+      }
+    ]
+
+  }
+  submitNote(){
+    if(this.newNote){
+      this.notes.push(
+        {
+          id: this.notes.length+1,
+          note: this.newNote
+        }
+      )
+      this.newNote = ""
+    }
+    else {
+      alert("please add note")
+    }
+  }
+
+  editNote(id: number){
+    this.newNote = this.notes.filter(x => x.id == id)[0].note; 
+    this.noteId = id; 
+    this.editStatus = true;
+
+  }
+  submitEditedNote(){
+    this.notes[this.notes.findIndex(x => x.id == this.noteId)].note = this.newNote;
+    this.editStatus = false;
+    this.newNote = ''
+  }
+
+  deleteNote(id: number){
+    var result = confirm("Sure you want to delete this?");
+    if (result) {
+      this.notes.splice(this.notes[this.notes.findIndex(x => x.id == id)], 1)
+    }
+  }
+
+  cancelEdit(){
+    this.newNote = '';
+    this.editStatus = false;
+  }
 }
